5c3210fa299b0ec7d72cab6fd356b62685758139
Raj Kanchan <iamraajkanchan@gmail.com>
2023-12-21 19:40:19 +0530

    Modifications made in BioMetricUtil.kt and MainActivity.kt

--- app/src/main/java/com/example/biometrics_android/MainActivity.kt
+++ app/src/main/java/com/example/biometrics_android/MainActivity.kt
@@ -1,27 +1,17 @@
 package com.example.biometrics_android
 
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.example.biometrics_android.util.BioMetricUtil
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() , View.OnClickListener
-{
-    private lateinit var bioMetricUtil : BioMetricUtil
-    override fun onCreate(savedInstanceState : Bundle?)
-    {
+class MainActivity : AppCompatActivity() {
+    private lateinit var bioMetricUtil: BioMetricUtil
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnLogin.setOnClickListener(this)
         bioMetricUtil = BioMetricUtil(this)
-    }
-
-    override fun onClick(view : View?)
-    {
-        if (view?.id == R.id.btnLogin)
-        {
+        btnLogin.setOnClickListener {
             bioMetricUtil.authenticate()
         }
     }
--- app/src/main/java/com/example/biometrics_android/util/BioMetricUtil.kt
+++ app/src/main/java/com/example/biometrics_android/util/BioMetricUtil.kt
@@ -13,62 +13,71 @@ import androidx.core.content.ContextCompat
 import com.example.biometrics_android.MainActivity
 import com.example.biometrics_android.R
 
-class BioMetricUtil(private val context : Context)
-{
-    private lateinit var biometricPrompt : BiometricPrompt
-    private lateinit var biometricPromptInfo : BiometricPrompt.PromptInfo
-    private val tvText : TextView = (context as MainActivity).findViewById(R.id.tvText)
-    private val btnLogin : Button = (context as MainActivity).findViewById(R.id.btnLogin)
+class BioMetricUtil(private val context: Context) {
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var biometricPromptInfo: BiometricPrompt.PromptInfo
+    private val tvText: TextView = (context as MainActivity).findViewById(R.id.tvText)
+    private val btnLogin: Button = (context as MainActivity).findViewById(R.id.btnLogin)
 
-    init
-    {
+    init {
         setBioMetric()
     }
 
-    private fun setBioMetric()
-    {
-        val biometricManager : BiometricManager = BiometricManager.from(context)
-        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG))
-        {
-            BiometricManager.BIOMETRIC_SUCCESS ->
-            {
-                tvText.text = "You can use the finger sensor to login."
+    private fun setBioMetric() {
+        val biometricManager: BiometricManager = BiometricManager.from(context)
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG)) {
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                tvText.text = context.getText(R.string.finger_print_enabled_message)
                 tvText.setTextColor(Color.parseColor("#FAFAFA"))
             }
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
-            {
-                tvText.text = "This device does not have a fingerprint scanner."
-            }
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
-            {
-                tvText.text = "The biometric sensor is currently unavailable."
+
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                tvText.text = context.getText(R.string.incompatible_device_message)
                 btnLogin.visibility = View.GONE
             }
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
-            {
-                tvText.text = "Your device doesn't have a fingerprint scanner."
+
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                tvText.text = context.getText(R.string.finger_print_disabled_message)
+                btnLogin.visibility = View.GONE
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                tvText.text = context.getText(R.string.finger_print_not_configured_message)
+                btnLogin.visibility = View.GONE
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_SECURITY_UPDATE_REQUIRED -> {
+                tvText.text = context.getText(R.string.security_update_message)
+                btnLogin.visibility = View.GONE
+            }
+
+            BiometricManager.BIOMETRIC_ERROR_UNSUPPORTED -> {
+                tvText.text = context.getText(R.string.finger_print_not_supported_message)
+                btnLogin.visibility = View.GONE
+            }
+
+            BiometricManager.BIOMETRIC_STATUS_UNKNOWN -> {
+                tvText.text = context.getText(R.string.finger_print_unknown_message)
+                btnLogin.visibility = View.GONE
             }
         }
 
         val executor = ContextCompat.getMainExecutor(context)
-        val callback = object : BiometricPrompt.AuthenticationCallback()
-        {
-            override fun onAuthenticationSucceeded(result : BiometricPrompt.AuthenticationResult)
-            {
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
                 super.onAuthenticationSucceeded(result)
-                Toast.makeText(context , "Login Successful" , Toast.LENGTH_LONG).show()
-                btnLogin.text = "Login Successful"
+                Toast.makeText(context, "Login Successful", Toast.LENGTH_LONG).show()
+                btnLogin.text = context.getText(R.string.login_successful)
             }
         }
-        biometricPrompt = BiometricPrompt(context as MainActivity , executor , callback)
+        biometricPrompt = BiometricPrompt(context as MainActivity, executor, callback)
 
-        biometricPromptInfo = BiometricPrompt.PromptInfo.Builder().setTitle("Chinky & Brothers")
+        biometricPromptInfo = BiometricPrompt.PromptInfo.Builder().setTitle("Shaw Info Solutions")
             .setDescription("Use your finger print to login").setNegativeButtonText("Cancel")
             .build()
     }
 
-    fun authenticate()
-    {
+    fun authenticate() {
         biometricPrompt.authenticate(biometricPromptInfo)
     }
 }
